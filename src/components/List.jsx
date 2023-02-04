2b9c872b8daf4b8a95511c45d6920086dde67031
Zuber Khan <50729008+ZUBERKHAN034@users.noreply.github.com>
2023-02-04 20:06:26 +0530

    Initial commit

---
+++ src/components/List.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function List() {
+  return (
+    <ul>
+      <li> apple</li>
+      <li> banana</li>
+      <li> mango</li>
+    </ul>
+  );
+}
+
+export default List;
